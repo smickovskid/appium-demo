fbcb033ccefbb9a79fca40876ef6d04acd84baf0
Damjan Smickovski <damjansmickovski@Damjans-MacBook-Pro-2.local>
2023-06-27 20:08:01 +0200

    Added Screens and config folders

---
+++ src/screens/Settings.ts
@@ -0,0 +1,23 @@
+
+
+
+export default class SettingsScreen {
+    public batteryButton = '//*[@text="Battery"]';
+
+    constructor() {
+        // Configuration
+    }
+
+    // Some example methods
+    public async openBatterySettings() {
+        throw new Error("Not implemented");
+    };
+
+    public async getBatteryLevel() {
+        throw new Error("Not implemented");
+    };
+}
+
+
+
+export const settingsScreen = new SettingsScreen();
--- src/tests/settingsTest.ts
+++ src/tests/settingsTest.ts
@@ -1,14 +1,16 @@
 
 const { remote } = require('webdriverio');
-import { getConfig } from '../../appium-conf';
+import SettingsScreen from '@screens/Settings';
+import { getConfig } from '@config/appium-conf';
 
 describe("Settings suite", async () => {
     const config = getConfig('android');
+    const settingsScreen = new SettingsScreen();
 
     it("Should open settings -> Battery", async () => {
         const driver = await remote(config);
         try {
-            const batteryItem = await driver.$('//*[@text="Battery"]');
+            const batteryItem = await driver.$(settingsScreen.batteryButton);
             await batteryItem.click();
         } finally {
             await driver.pause(1000);
